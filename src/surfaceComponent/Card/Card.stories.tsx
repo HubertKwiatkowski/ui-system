1deb20e2b6de86a1c4480bbae47b630a4d2fc60e
Hubert <h.kwiatkowski@hotmail.com>
2023-03-08 13:43:48 +0100

    card done

--- src/surfaceComponent/Card/Card.stories.tsx
+++ src/surfaceComponent/Card/Card.stories.tsx
@@ -2,14 +2,13 @@ import { Meta, Story } from "@storybook/react";
 import { Card } from "./Card";
 import * as React from "react";
 
-
 export default {
   component: Card,
   argTypes: {},
 } as Meta;
 
-export const MultiCard: Story = (args) => <Card {...args}></Card>
+export const MultiCard: Story = (args) => <Card {...args}></Card>;
 
 MultiCard.args = {
   fullWidth: false,
-}
+};
--- src/surfaceComponent/Card/Card.tsx
+++ src/surfaceComponent/Card/Card.tsx
@@ -1,41 +1,74 @@
 import { PropsWithChildren } from "react";
 import * as React from "react";
 import { Button } from "../../inputs";
-import smallImage from "../../static/images/card/card-small.png"
-import bigImage from "../../static/images/card/card-big.png"
+import smallImage from "../../static/images/card/card-small.png";
+import bigImage from "../../static/images/card/card-big.png";
 import * as styles from "./Card.module.css";
 
 interface CardProps {
   fullWidth?: boolean;
 }
 
-export const Card = ({
-  fullWidth = false
-}: PropsWithChildren<CardProps>) => {
-  
-  const imageUrl = fullWidth ? bigImage : smallImage
+export const Card = ({ fullWidth = false }: PropsWithChildren<CardProps>) => {
+  const handlePrimaryButton = () => {};
+  const handleSecondaryButton = () => {};
 
-  const getIfFullWidth = fullWidth ? styles.fullWidth : ""
+  const imageUrl = fullWidth ? bigImage : smallImage;
+
+  const getIfFullWidthCard = fullWidth ? styles.fullWidth : "";
+
+  const getIfFullWidhtCardContent = fullWidth ? styles.fullWidthCardContent : ""
+
+  const getIfFullWidthButtonWrapper = fullWidth ? styles.buttonWrapperFullWidth : "";
 
   const cardDynamicClasses = [
-    styles.cardWrapper,
-    getIfFullWidth,
+    styles.cardWrapper, 
+    getIfFullWidthCard
+  ].join(" ");
+
+  const cardContentDynamicClasses = [
+    styles.cardContent,
+    getIfFullWidhtCardContent,
   ].join(" ")
-  
+
+  const buttonWrapperDynamicClasses = [
+    styles.buttonWrapper, 
+    getIfFullWidthButtonWrapper,
+  ].join(" ")
+
   return (
     <div className={cardDynamicClasses}>
-      
-      <img src={imageUrl} alt=""/>
-      <div className={styles.cardContent}>
+      <img src={imageUrl} alt="" />
+      <div className={cardContentDynamicClasses}>
         <div>
           <h3>Card Name</h3>
-          <p>Card description goes here and it can be as long as it needs and so on.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim harum porro sunt nulla natus iusto necessitatibus debitis rem consequuntur repellendus, illum quae. Tempora quis dolor sunt, earum, amet repellat tenetur dicta porro ab voluptatum, animi ex aliquid corporis quas quasi iusto in eos odit similique facere tempore deserunt impedit exercitationem. Nesciunt deleniti perspiciatis labore modi, neque distinctio pariatur quisquam officiis fugiat ut voluptatem dolores assumenda facere molestiae omnis, ducimus error animi eius praesentium doloremque possimus minus. Blanditiis voluptates magnam sunt voluptatem enim hic reprehenderit, nam obcaecati tenetur architecto recusandae sit libero, laboriosam nemo iusto ad repellendus. In ad maiores id?
+          </p>
         </div>
-        <div className={styles.buttonWrapper}>
-          <Button variant={"primary"}>Click me</Button>
-          <Button variant={"secondary"}>Click me</Button>
+        <div className={buttonWrapperDynamicClasses}>
+          <div>
+            <Button 
+              type="button"
+              variant={"primary"}
+              onClick={handlePrimaryButton}
+            >
+              Primary button
+            </Button>
+          </div>
+          <div>
+            <Button
+              type="button"
+              variant={"secondary"}
+              onClick={handleSecondaryButton}
+
+            >
+              Secondary button
+            </Button>
+
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
