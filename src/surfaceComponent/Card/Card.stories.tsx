95ea42f06194d3c31a4232892b71d8dce7f26e06
Hubert <h.kwiatkowski@hotmail.com>
2023-03-15 10:40:52 +0100

    fix: fixed empty handlers

--- src/surfaceComponent/Card/Card.stories.tsx
+++ src/surfaceComponent/Card/Card.stories.tsx
@@ -1,13 +1,23 @@
 import { Meta, Story } from "@storybook/react";
 import { Card } from "./Card";
 import * as React from "react";
+import { Button } from "../../inputs";
 
 export default {
   component: Card,
   argTypes: {},
 } as Meta;
 
-export const MultiCard: Story = (args) => <Card {...args}></Card>;
+export const MultiCard: Story = (args) => (
+  <Card
+    fullWidth={true}
+    {...args}
+  >
+    {/* <CardImage url={imageUrl}> */}
+    {/* <CardContent> */}
+      <Button variant={"primary"}></Button>
+    {/* </CardContent> */}
+  </Card>);
 
 MultiCard.args = {
   fullWidth: false,
--- src/surfaceComponent/Card/Card.tsx
+++ src/surfaceComponent/Card/Card.tsx
@@ -1,57 +1,74 @@
 import { PropsWithChildren } from "react";
 import * as React from "react";
 import { Button } from "../../inputs";
-import smallImage from "../../static/images/card/card-small.png";
-import bigImage from "../../static/images/card/card-big.png";
+import imageUrl from "../../static/images/card/card-big.png";
 import * as styles from "./Card.module.css";
 
 interface CardProps {
+  onClick(): void;
   fullWidth?: boolean;
 }
 
-export const Card = ({ fullWidth = false }: PropsWithChildren<CardProps>) => {
-  const handlePrimaryButton = () => {};
-  const handleSecondaryButton = () => {};
+const CardImage = ({}) => {
+  return (
+    <img src={imageUrl} alt="" />
+  )
+}
 
-  const imageUrl = fullWidth ? bigImage : smallImage;
+export const Card = ({ 
+  fullWidth = false,
+  ...props
+}: PropsWithChildren<CardProps>) => {
 
   const getIfFullWidthCard = fullWidth ? styles.fullWidth : "";
 
-  const getIfFullWidhtCardContent = fullWidth ? styles.fullWidthCardContent : ""
+  const getIfFullWidhtCardContent = fullWidth
+    ? styles.fullWidthCardContent
+    : "";
 
-  const getIfFullWidthButtonWrapper = fullWidth ? styles.buttonWrapperFullWidth : "";
+  const getIfFullWidthButtonWrapper = fullWidth
+    ? styles.buttonWrapperFullWidth
+    : "";
 
-  const cardDynamicClasses = [
-    styles.cardWrapper, 
-    getIfFullWidthCard
-  ].join(" ");
+  const cardDynamicClasses = [styles.cardWrapper, getIfFullWidthCard].join(" ");
 
   const cardContentDynamicClasses = [
     styles.cardContent,
     getIfFullWidhtCardContent,
-  ].join(" ")
+  ].join(" ");
 
   const buttonWrapperDynamicClasses = [
-    styles.buttonWrapper, 
+    styles.buttonWrapper,
     getIfFullWidthButtonWrapper,
-  ].join(" ")
+  ].join(" ");
 
   return (
     <div className={cardDynamicClasses}>
-      <img src={imageUrl} alt="" />
+      <CardImage />
       <div className={cardContentDynamicClasses}>
         <div>
           <h3>Card Name</h3>
           <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim harum porro sunt nulla natus iusto necessitatibus debitis rem consequuntur repellendus, illum quae. Tempora quis dolor sunt, earum, amet repellat tenetur dicta porro ab voluptatum, animi ex aliquid corporis quas quasi iusto in eos odit similique facere tempore deserunt impedit exercitationem. Nesciunt deleniti perspiciatis labore modi, neque distinctio pariatur quisquam officiis fugiat ut voluptatem dolores assumenda facere molestiae omnis, ducimus error animi eius praesentium doloremque possimus minus. Blanditiis voluptates magnam sunt voluptatem enim hic reprehenderit, nam obcaecati tenetur architecto recusandae sit libero, laboriosam nemo iusto ad repellendus. In ad maiores id?
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim harum
+            porro sunt nulla natus iusto necessitatibus debitis rem consequuntur
+            repellendus, illum quae. Tempora quis dolor sunt, earum, amet
+            repellat tenetur dicta porro ab voluptatum, animi ex aliquid
+            corporis quas quasi iusto in eos odit similique facere tempore
+            deserunt impedit exercitationem. Nesciunt deleniti perspiciatis
+            labore modi, neque distinctio pariatur quisquam officiis fugiat ut
+            voluptatem dolores assumenda facere molestiae omnis, ducimus error
+            animi eius praesentium doloremque possimus minus. Blanditiis
+            voluptates magnam sunt voluptatem enim hic reprehenderit, nam
+            obcaecati tenetur architecto recusandae sit libero, laboriosam nemo
+            iusto ad repellendus. In ad maiores id?
           </p>
         </div>
         <div className={buttonWrapperDynamicClasses}>
           <div>
-            <Button 
+            <Button
               type="button"
               variant={"primary"}
-              onClick={handlePrimaryButton}
+              onClick={() => {props.onClick}}
             >
               Primary button
             </Button>
@@ -60,12 +77,10 @@ export const Card = ({ fullWidth = false }: PropsWithChildren<CardProps>) => {
             <Button
               type="button"
               variant={"secondary"}
-              onClick={handleSecondaryButton}
-
+              onClick={() => {props.onClick}}
             >
               Secondary button
             </Button>
-
           </div>
         </div>
       </div>
