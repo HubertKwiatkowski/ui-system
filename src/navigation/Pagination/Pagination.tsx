443e1e4174ed9242da4abb1cb09e6ea5354433aa
Hubert <h.kwiatkowski@hotmail.com>
2023-03-28 17:50:23 +0200

    feat: redone base pagination code

--- src/navigation/Pagination/Pagination.tsx
+++ src/navigation/Pagination/Pagination.tsx
@@ -1,100 +1,63 @@
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useState } from "react";
 import * as React from "react";
-import { ArrowLeft, ArrowRight } from "../../icons";
-import { usePagination, DOTS } from "./UsePagination";
-import classnames from "classnames";
+import { ArrowLeftButton } from "./ArrowLeftButton";
+import { ArrowRightButton } from "./ArrowRightButton";
+import { Ellipsis } from "./Ellipsis";
 
 import * as styles from "./Pagination.module.css";
 
 interface PaginationProps {
-  maxPages: number;
   currentPage: number;
+  maxPages: number;
   onChange(value: number): void;
 }
 
 export const Pagination = ({
   ...props
 }: PropsWithChildren<PaginationProps>) => {
-  const currentPage = props.currentPage;
   const maxPages = props.maxPages;
-
-  const paginationRange = usePagination({
-    currentPage,
-    maxPages,
-  });
-
-  if (
-    currentPage <= 0 ||
-    currentPage > maxPages ||
-    paginationRange.length < 2
-  ) {
-    return null;
-  }
-
-  const paginationRangeLength = paginationRange.length;
-
-  const lastPage = paginationRange[paginationRangeLength - 1];
-
-  const leftIsDisabled = currentPage === 1 ? styles.disabled : "";
-  const rightIsDisabled = currentPage === lastPage ? styles.disabled : "";
-
-  const arrowLeft = [styles.baseIcon, styles.leftIcon, leftIsDisabled].join(
-    " "
-  );
-  const arrowRight = [styles.baseIcon, styles.rightIcon, rightIsDisabled].join(
-    " "
+  const pageNumbers = Array.from(Array(maxPages), (_, i) => i + 1);
+  const [currentPageIndex, setCurrentPageIndex] = useState<number>(
+    props.currentPage
   );
 
-  const paginationItemNumber = [styles.paginationItem].join(" ");
-
-  const paginationItemDots = [styles.paginationItem, styles.dots].join(" ");
-
-  const onNext = () => {
-    props.onChange(currentPage + 1);
-    console.log(currentPage);
+  const handlePrevClick = () => {
+    if (currentPageIndex > 1) {
+      setCurrentPageIndex(currentPageIndex - 1);
+      props.onChange();
+      console.log(currentPageIndex);
+    }
   };
 
-  const onPrevious = () => {
-    props.onChange(currentPage - 1);
-    console.log(currentPage);
+  const handleNextClick = () => {
+    if (currentPageIndex < maxPages) {
+      setCurrentPageIndex(currentPageIndex + 1);
+      props.onChange();
+      console.log(currentPageIndex);
+    }
+  };
+
+  const handleSetPage = () => {
+    props.onChange();
+    console.log(currentPageIndex);
   };
 
   return (
-    <ul className={styles.mainWrapper}>
-      <li>
-        <div
-          className={classnames(arrowLeft, { disabled: currentPage === 1 })}
-          onClick={onPrevious}
-        >
-          {<ArrowLeft />}
-        </div>
-      </li>
-      {paginationRange.map((pageNumber) => {
-        if (pageNumber === DOTS) {
-          return <li key={pageNumber} className={paginationItemDots}>&#8230;</li>;
-        }
-        return (
-          <li
-            key={pageNumber}
-            className={classnames(paginationItemNumber, {
-              selected: pageNumber === currentPage,
-            })}
-            onClick={() => props.onChange(pageNumber)}
-          >
-            {pageNumber}
-          </li>
-        );
-      })}
-      <li>
-        <div
-          className={classnames(arrowRight, {
-            disabled: currentPage === lastPage,
-          })}
-          onClick={onNext}
-        >
-          <ArrowRight />
-        </div>
-      </li>
-    </ul>
+    <div className={styles.mainWrapper}>
+      <ArrowLeftButton
+        onClick={handlePrevClick}
+        disabled={currentPageIndex === 1}
+      />
+      <Ellipsis
+        onClick={() => handleSetPage()}
+        pageNumbers={pageNumbers}
+        currentPage={currentPageIndex}
+        setCurrentPage={setCurrentPageIndex}
+      />
+      <ArrowRightButton
+        onClick={handleNextClick}
+        disabled={currentPageIndex === maxPages}
+      />
+    </div>
   );
 };
