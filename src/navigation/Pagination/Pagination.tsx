ef0d8934c4489879e8f91d0f98b9d9a0a18dd133
Hubert <h.kwiatkowski@hotmail.com>
2023-04-18 20:30:19 +0200

    fix: redone pagination

--- src/navigation/Pagination/Pagination.tsx
+++ src/navigation/Pagination/Pagination.tsx
@@ -1,78 +1,159 @@
-import { PropsWithChildren, useState } from "react";
-import * as React from "react";
-import { ArrowLeftButton } from "./ArrowLeftButton";
-import { ArrowRightButton } from "./ArrowRightButton";
-import { Ellipsis } from "./Ellipsis";
-import classNames from "classnames/bind";
+import React from "react";
 
-import * as styles from "./Pagination.module.css";
+interface PaginationItemProps {
+  disabled?: boolean;
+  onClick: () => void;
+  children: React.ReactNode;
+}
 
-const cx = classNames.bind(styles);
+const PaginationItem: React.FC<PaginationItemProps> = ({
+  disabled = false,
+  onClick,
+  children,
+}) => {
+  return (
+    <li className={`page-item ${disabled ? "disabled" : ""}`}>
+      <button className="page-link" onClick={onClick} disabled={disabled}>
+        {children}
+      </button>
+    </li>
+  );
+};
+
+interface PaginationButtonProps {
+  active?: boolean;
+  onClick: () => void;
+  children: React.ReactNode;
+}
+
+const PaginationButton: React.FC<PaginationButtonProps> = ({
+  active = false,
+  onClick,
+  children,
+}) => {
+  return (
+    <li className={`page-item ${active ? "active" : ""}`}>
+      <button className="page-link" onClick={onClick}>
+        {children}
+      </button>
+    </li>
+  );
+};
 
 interface PaginationProps {
   currentPage: number;
   maxPages: number;
-  onChange(value: number): void;
+  onChange: (value: number) => void;
 }
-
-export const Pagination = ({
-  ...props
-}: PropsWithChildren<PaginationProps>) => {
-  const maxPages = props.maxPages;
-  const pageNumbers = Array.from(Array(maxPages), (_, i) => i + 1);
-  const [currentPageIndex, setCurrentPageIndex] = useState<number>(
-    props.currentPage
-  );
-
-  const handlePrevClick = () => {
-    if (currentPageIndex > 1) {
-      setCurrentPageIndex(currentPageIndex - 1);
-      props.onChange();
-      console.log(currentPageIndex);
+export const Pagination: React.FC<PaginationProps> = ({
+  currentPage,
+  maxPages,
+  onChange,
+}) => {
+  const handlePageChange = (page: number) => {
+    if (page >= 1 && page <= maxPages) {
+      onChange(page);
     }
   };
-
-  const handleNextClick = () => {
-    if (currentPageIndex < maxPages) {
-      setCurrentPageIndex(currentPageIndex + 1);
-      props.onChange();
-      console.log(currentPageIndex);
-    }
-  };
-
-  const handleSetPage = () => {
-    props.onChange();
-    console.log(currentPageIndex);
-  };
-
-  const arrowLeftClass = cx({
-    disabledArrow: currentPageIndex === 1,
-  });
-
-  const arrowRightClass = cx({
-    disabledArrow: currentPageIndex === maxPages,
-  });
 
   return (
-    <div className={styles.mainWrapper}>
-      <div className={arrowLeftClass}>
-        <ArrowLeftButton
-          onClick={handlePrevClick}
-          disabled={currentPageIndex === 1}
-        />
-      </div>
-      <Ellipsis
-        onClick={() => handleSetPage()}
-        pageNumbers={pageNumbers}
-        currentPage={currentPageIndex}
-        setCurrentPage={setCurrentPageIndex}
-      />
-      <div className={arrowRightClass}>
-        <ArrowRightButton
-          onClick={handleNextClick}
-          disabled={currentPageIndex === maxPages}
-        />
-      </div>
-    </div>
+    <nav>
+      <ul className="pagination">
+        <PaginationItem
+          disabled={currentPage === 1}
+          onClick={() => handlePageChange(currentPage - 1)}
+        >
+          Previous
+        </PaginationItem>
+        {[...Array(maxPages)].map((_, i) => (
+          <PaginationButton
+            key={i + 1}
+            active={currentPage === i + 1}
+            onClick={() => handlePageChange(i + 1)}
+          >
+            {i + 1}
+          </PaginationButton>
+        ))}
+        <PaginationItem
+          disabled={currentPage === maxPages}
+          onClick={() => handlePageChange(currentPage + 1)}
+        >
+          Next
+        </PaginationItem>
+      </ul>
+    </nav>
   );
 };
+
+// import { PropsWithChildren, useState } from "react";
+// import * as React from "react";
+// import { ArrowLeftButton } from "./ArrowLeftButton";
+// import { ArrowRightButton } from "./ArrowRightButton";
+// import { Ellipsis } from "./Ellipsis";
+// import classNames from "classnames/bind";
+//
+// import * as styles from "./Pagination.module.css";
+//
+// const cx = classNames.bind(styles);
+//
+// interface PaginationProps {
+//   currentPage: number;
+//   maxPages: number;
+//   onChange(value: number): void;
+// }
+//
+// export const Pagination = ({
+//   ...props
+// }: PropsWithChildren<PaginationProps>) => {
+//   const maxPages = props.maxPages;
+//   const pageNumbers = Array.from(Array(maxPages), (_, i) => i + 1);
+//   const [currentPageIndex, setCurrentPageIndex] = useState<number>(
+//     props.currentPage
+//   );
+//
+//   const handlePrevClick = () => {
+//     if (currentPageIndex > 1) {
+//       setCurrentPageIndex(currentPageIndex - 1);
+//       console.log(currentPageIndex);
+//     }
+//   };
+//
+//   const handleNextClick = () => {
+//     if (currentPageIndex < maxPages) {
+//       setCurrentPageIndex(currentPageIndex + 1);
+//       console.log(currentPageIndex);
+//     }
+//   };
+//
+//   const arrowLeftClass = cx({
+//     disabledArrow: currentPageIndex === 1,
+//   });
+//
+//   const arrowRightClass = cx({
+//     disabledArrow: currentPageIndex === maxPages,
+//   });
+//
+//   return (
+//     <div className={styles.mainWrapper}>
+//       <div className={arrowLeftClass}>
+//         <ArrowLeftButton
+//           onClick={handlePrevClick}
+//           disabled={currentPageIndex === 1}
+//         />
+//       </div>
+//       <Ellipsis
+//         onChange={() => {
+//           setCurrentPageIndex(currentPageIndex);
+//         }}
+//         pageNumbers={pageNumbers}
+//         currentPage={currentPageIndex}
+//       />
+//       <div className={arrowRightClass}>
+//         <ArrowRightButton
+//           onClick={handleNextClick}
+//           disabled={currentPageIndex === maxPages}
+//         />
+//       </div>
+//     </div>
+//   );
+// };
