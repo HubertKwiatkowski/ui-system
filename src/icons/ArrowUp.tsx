ffa1da793c1c7df2caa9c79a18c4355da8edb75e
Hubert <h.kwiatkowski@hotmail.com>
2023-06-05 19:29:00 +0200

    chore: accordion

---
+++ src/icons/ArrowUp.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+export const ArrowUp = () => (
+  <svg
+    width="12"
+    height="8"
+    viewBox="0 0 12 8"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M1.41 7.705L-2.62268e-07 6.295L6 0.294998L12 6.295L10.59 7.705L6 3.125L1.41 7.705Z"
+      fill="#1C1B1F"
+    />
+  </svg>
+);
